8f201da072423c4e70eeeace6c9980597c9d78cc
Matheus Giacomel <matheushdpg@gmail.com>
2023-08-22 01:39:25 -0300

    Adicionado arquivo properties

--- connection.py
+++ connection.py
@@ -1,12 +1,22 @@
 import psycopg2
+import configparser
 
 
 def get_connection():
+    config = configparser.ConfigParser()
+    config.read("config.properties")
+
+    database_name = config.get("database", "database")
+    database_user = config.get("database", "user")
+    database_password = config.get("database", "password")
+    database_host = config.get("database", "host")
+    database_port = config.get("database", "port")
+
     conn = psycopg2.connect(
-        database="testejpa",
-        user="postgres",
-        password="root",
-        host="127.0.0.1",
-        port="5432",
+        database=database_name,
+        user=database_user,
+        password=database_password,
+        host=database_host,
+        port=database_port,
     )
     return conn
